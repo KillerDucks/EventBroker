06995f50dae97ffb5a4e4226ee9920418f27dd23
KillerDucks <x86bit-server@outlook.com>
2019-11-15 00:51:34 +0000

    Added the newer auth check using the xAuth Server

--- WhatData_WAPI/Routes/utils_route.js
+++ WhatData_WAPI/Routes/utils_route.js
@@ -5,17 +5,30 @@
  * Version: 1.0.0
  */
 
-// Debug [REMOVE]
-const sKey = "SecureToken";
+// Import the HTTP lib
+const http = require('http');
 
 // This function check the security description of the request and validates it
-function Check_Security(Security) {
-    // This is only for debugging [TODO] Minimal change to JWT Validation
-    if(Security.Key == sKey)
-    {
-        return true;
-    }
-    return false;
+function Check_Security(Security, resp = {}) {
+    // This will verify against a single Authentication source [TODO] Incorporate with EventBus/AutoDiscovery for Hoz scaling.
+    // [POC] The Docker DNS will handle the name resolution for the single Auth Broker [Debug] Using Docker Host IP for now
+    http.get("http://localhost:9090/token/verify", {headers: {"Authorization": Security}}, (res) => {
+        res.on("data", (data) => {
+            let r = JSON.parse(Buffer.from(data).toString("utf8"));
+            // [TODO] Change this valid detection Strings are bad
+            if(r.Data == "Token is valid")
+            {
+                // Token is Valid
+                console.log("Valid")
+                resp(1);
+            }
+            else
+            {
+                console.log("!Valid")
+                resp(-1);
+            }
+        });
+    }).on("error", (e) => {console.log(e)});
 }
 
 // Return simple error messages
@@ -37,7 +50,7 @@ function Return_Error_Msg(res, error = {}) {
 
 // Return simple payloads
 function Return_Response(res, response = {}) {
-    res.status(403).json({
+    res.status(200).json({
         "Timestamp": Date.now(),
         "Security": {
             "Type": "None",
